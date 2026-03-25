cd2e8d4ae8753741717b509f7a4fa948dd2aa0fd
shitiandmw <lushitian502@gmail.com>
2026-03-25 11:43:39 +0800

    auto-commit: uncommitted changes from session 1ad6229d

--- src/api/admin/brands/validators.ts
+++ src/api/admin/brands/validators.ts
@@ -4,12 +4,14 @@ export const PostAdminCreateBrand = z.object({
   name: z.string().min(1),
   description: z.string().optional(),
   logo_url: z.string().url().optional(),
+  origin: z.string().optional(),
 })
 
 export const PostAdminUpdateBrand = z.object({
   name: z.string().min(1).optional(),
   description: z.string().optional(),
   logo_url: z.string().url().optional(),
+  origin: z.string().optional(),
 })
 
 export type PostAdminCreateBrandType = z.infer<typeof PostAdminCreateBrand>
